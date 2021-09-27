2a9dfb05ff6e1cee9939539122efa375e5b14726
Delfajor <88336333+delfinajorge@users.noreply.github.com>
2021-09-27 12:04:53 -0300

    Filtros para editor

--- archivo.js
+++ archivo.js
@@ -18,6 +18,36 @@ const mostrarTexto=()=>{
     document.getElementById("columnaTexto").classList.remove("ocultar")
 }
 
+/*********** filtros **********/
+const actualizarFiltros = () => {
+    const brightness = document.getElementById('brightness').value;
+    const opacity = document.getElementById('opacity').value;
+    const contrast = document.getElementById('contrast').value;
+    const blur = document.getElementById('blur').value;
+    const grayscale = document.getElementById('grayscale').value;
+    const sepia = document.getElementById('sepia').value;
+    const hue = document.getElementById('hue').value;
+    const saturate = document.getElementById('saturate').value;
+    const invert = document.getElementById('invert').value;
+  
+    $(
+      'imagememe'
+    ).style.filter = `brightness(${brightness}) opacity(${opacity}) blur(${blur}px) contrast(${contrast}%) grayscale(${grayscale}%) hue-rotate(${hue}deg) sepia(${sepia}%) saturate(${saturate}%) invert(${invert})`
+  }
+  
+  const reestrablecerFiltros = () => {
+    document.getElementById('brightness').value = 1
+    document.getElementById('opacity').value = 1
+    document.getElementById('contrast').value = 100
+    document.getElementById('blur').value = 0
+    document.getElementById('grayscale').value = 0
+    document.getElementById('sepia').value = 0
+    document.getElementById('hue').value = 0
+    document.getElementById('saturate').value = 100
+    document.getElementById('invert').value = 0
+  
+    actualizarFiltros()
+  }
 
 
 /*****************para la cruz de cerrar ventana**********/    
